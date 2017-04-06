dc7ec053be701d69c99e140826ef0fca67a80d79
Clash of Lights <clashoflightsdonate@gmail.com>
2017-04-06 17:44:16 +0200

    removed berkans shit...

--- Ultrapowa Clash Server/Packets/Crypto.cs
+++ Ultrapowa Clash Server/Packets/Crypto.cs
@@ -8,9 +8,6 @@ namespace UCS.Packets
         internal byte[] RNonce;
         internal byte[] PublicKey;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Crypto"/> class.
-        /// </summary>
         internal Crypto()
         {
             this.PublicKey = new byte[32];
@@ -18,10 +15,6 @@ namespace UCS.Packets
             this.RNonce = new byte[24];
         }
 
-        /// <summary>
-        /// Exécute les tâches définies par l'application associées 
-        /// à la libération ou à la redéfinition des ressources non managées.
-        /// </summary>
         void IDisposable.Dispose()
         {
             this.SNonce = null;
